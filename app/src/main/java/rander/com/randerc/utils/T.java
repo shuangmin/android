c38090bbd57d6f18c0b5c33563f54b339cfe9545
shuangmin <konkacsm@gmail.com>
2016-03-24 00:07:20 +0800

    10大方便开发的工具类提交

---
+++ app/src/main/java/rander/com/randerc/utils/T.java
@@ -0,0 +1,93 @@
+package rander.com.randerc.utils;
+
+/**
+ * Created by rander on 16-3-23.
+ */
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import rander.com.randerc.app.MyApplication;
+
+/**
+ * Toast统一管理类
+ */
+public class T {
+
+
+    private T() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    public static boolean isShow = true;
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, CharSequence message) {
+        if (isShow)
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, int message) {
+        if (isShow)
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, CharSequence message) {
+        if (isShow)
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, int message) {
+        if (isShow)
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, CharSequence message, int duration) {
+        if (isShow)
+            Toast.makeText(context, message, duration).show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, int message, int duration) {
+        if (isShow)
+            Toast.makeText(context, message, duration).show();
+    }
+}
